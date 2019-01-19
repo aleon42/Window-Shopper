1af06073e741638e9d0371f7f33ce64e9b431d2e
Albert Leon <aleon42@me.com>
2019-01-19 17:46:52 -0500

    customized view

--- Window-Shopper/window-shopper/views/currencyTextField.swift
+++ Window-Shopper/window-shopper/views/currencyTextField.swift
@@ -8,26 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class currencyTextField: UITextField {
-
+    
+    override func prepareForInterfaceBuilder () {
+        customizedView()
+    }
     override func awakeFromNib() {
-        super.awakeFromNib()
+       super.awakeFromNib()
+        customizedView()
+    }
+   
+    func customizedView (){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
         if let p = placeholder {
-        let place = NSAttributedString (string: p, attributes:[.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-        attributedPlaceholder = place
-        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            let place = NSAttributedString (string: p, attributes:[.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
+
