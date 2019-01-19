327189e0e5b3d27714f8675b435db8f9eea138d2
Albert Leon <aleon42@me.com>
2019-01-19 17:34:22 -0500

    first button

---
+++ Window-Shopper/window-shopper/views/currencyTextField.swift
@@ -0,0 +1,33 @@
+//
+//  currencyTextField.swift
+//  window-shopper
+//
+//  Created by Albert Leon on 1/19/19.
+//  Copyright © 2019 Albert Leon. All rights reserved.
+//
+
+import UIKit
+
+class currencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+        let place = NSAttributedString (string: p, attributes:[.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        attributedPlaceholder = place
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
